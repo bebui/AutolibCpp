08a8632479c58b6530e83a9a4e898bf42540eaff
Julien Menana <julien@menana.fr>
2011-12-25 14:25:14 +0100

    Automaton base class

---
+++ src/struct/Automaton.cc
@@ -0,0 +1,21 @@
+/*
+ * Automaton.cc
+ *
+ *  Created on: Dec 25, 2011
+ *      Author: julien
+ */
+
+#include "Automaton.hh"
+
+namespace structure {
+
+Automaton::Automaton() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Automaton::~Automaton() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace structure */
---
+++ src/struct/Automaton.hh
@@ -0,0 +1,20 @@
+/*
+ * Automaton.hh
+ *
+ *  Created on: Dec 25, 2011
+ *      Author: julien
+ */
+
+#ifndef AUTOMATON_HH_
+#define AUTOMATON_HH_
+
+namespace structure {
+
+class Automaton {
+public:
+	Automaton();
+	virtual ~Automaton();
+};
+
+} /* namespace structure */
+#endif /* AUTOMATON_HH_ */
