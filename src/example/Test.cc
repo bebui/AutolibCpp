15012b90aba851a50505e626cdb1438ab863a75f
Julien Menana <julien@menana.fr>
2011-12-25 16:39:24 +0100

    try cmake

---
+++ src/example/Test.cc
@@ -0,0 +1,22 @@
+/*
+ * Test.cc
+ *
+ *  Created on: Dec 25, 2011
+ *      Author: julien
+ */
+
+#include "../struct/Automaton.hh"
+
+using namespace structure;
+
+int main()
+{
+	Automaton<char> automate;
+
+	automate.addState("toto");
+
+	return 0;
+}
+
+
+
--- src/struct/Automaton.hh
+++ src/struct/Automaton.hh
@@ -8,12 +8,44 @@
 #ifndef AUTOMATON_HH_
 #define AUTOMATON_HH_
 
+#include <boost/graph/adjacency_list.hpp>
+#include <string>
+
 namespace structure {
 
+using namespace boost;
+using std::string;
+
+struct symbol_t {
+  typedef edge_property_tag kind;
+};
+struct name_t {
+  typedef vertex_property_tag kind;
+};
+
+template < typename S >
 class Automaton {
 public:
+	typedef property<symbol_t,S> EdgeProperty;
+	typedef property<name_t,string> VertexProperty;
+
+	typedef adjacency_list<vecS, vecS, directedS, VertexProperty, EdgeProperty> Graph;
+	typedef Graph::vertex_descriptor State;
+
 	Automaton();
 	virtual ~Automaton();
+
+	void addState(string name_p = "")
+	{
+		State newState = add_vertex(graph_m);
+		graph_m[newState] = name_p;
+	}
+
+
+
+private:
+	Graph graph_m;
+
 };
 
 } /* namespace structure */
